af7f83b3dbde895dc6044747839b52e5a32ec2d3
Tino Selic <tino.selic@gmail.com>
2022-12-07 17:20:25 +0200

    Alarm corrected.

--- physical-computing-final-project/physical-computing-final-project.ino
+++ physical-computing-final-project/physical-computing-final-project.ino
@@ -7,8 +7,45 @@
 #include <Wire.h>
 #include <RTClib.h>
 
+// Libraries for the OLED display
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 // Define an object of RTC_DS1307 class
 RTC_DS1307 RTC;
+
+// OLED display width and height in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
 
 // Maximum number of digits that can be dialled (Time: hh:mm = 4), must be static
 #define MAXDIGITS 4
